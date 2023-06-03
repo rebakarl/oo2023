3def37a8585efa988f122cfbb0e5d692573499f5
rebakarl <117148135+rebakarl@users.noreply.github.com>
2023-06-03 09:42:01 +0300

    .

--- 11_proovikontrolltoo2/proovikt2/src/main/java/ee/karl/proovikt2/toiduaine.java
+++ 11_proovikontrolltoo2/proovikt2/src/main/java/ee/karl/proovikt2/toiduaine.java
@@ -1,21 +1,21 @@
 package ee.karl.proovikt2;
 
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.persistence.Entity;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @Entity
-@Id
 
 public class toiduaine {
+    @Id
     private Long id;
     private String nimetus;
     private int valk;
